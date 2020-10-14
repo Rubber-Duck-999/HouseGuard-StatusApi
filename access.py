0cfc018b3b8a55db717856be4f0cafb8a678dfa3
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2020-10-14 19:26:50 +0100

    PutItem creation - testing on wrong machine with no keys so could be failure

--- access.py
+++ access.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import uuid
 
 class Access_Db():
 
@@ -12,6 +13,27 @@ class Access_Db():
     def createStatus(self, status):
         self.status = status
         print("Creating record in db")
+        print(str(self.status)
+        if not dynamodb:
+            dynamodb = boto3.client('dynamodb')
+
+        table = dynamodb.Table('StatusId')
+        response = table.put_item(
+        Item={
+                'StatusId': str(uuid.uuidv4()),
+                'AlarmStatus': 'ON',
+                'MemoryLeft': '0',
+                'DevicesActive': '5',
+                'TimeLastEscConnected': '2020',
+                'LastMotionDetected': '2020',
+                'LastAccessGranted': '2020',
+                'LastAccessBlocked': '2020',
+                'LastUser': 'Admin'
+            }
+        )
+        return response
 
     def getStatus(self, status_request):
         print("Creating query for getting status record")
+
+    
